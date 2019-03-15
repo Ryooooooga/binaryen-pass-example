babf2d051e27740124c7143db3041e775bfbdeed
Ryooooooga <ryoga_314@yahoo.co.jp>
2019-03-15 17:24:13 +0900

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Binaryen を用いた WebAssembly カスタムパスの作成例
+
+注意点:
+
+1. `wasm::ModuleReader::read*()`はファイルが読めないと`exit(1)`する。
+
+2. `wasm::ModuleReader::read*()`はファイルの形式が間違っていると`wasm::ParseException`を投げるがこれは`std::except`を継承していない。
+
+3. `wasm::ModuleReader::read()/readText()`で読める WebAssembly テキストファイルは`.wast`形式であり、`.wat`形式とは互換性がない。
--- binaryen.cmake
+++ binaryen.cmake
@@ -1,9 +1,3 @@
-if (WIN32)
-    set(binaryen_BUILD_STATIC_LIB OFF)
-else ()
-    set(binaryen_BUILD_STATIC_LIB ON)
-endif ()
-
 ExternalProject_Add(
     binaryen
     URL https://github.com/WebAssembly/binaryen/archive/version_72.zip
@@ -11,7 +5,7 @@ ExternalProject_Add(
     INSTALL_COMMAND ""
     TEST_COMMAND ""
     CMAKE_ARGS
-        -DBUILD_STATIC_LIB=${binaryen_BUILD_STATIC_LIB}
+        -DBUILD_STATIC_LIB=OFF
         -DCMAKE_BUILD_TYPE=Release
 )
 
@@ -20,23 +14,14 @@ ExternalProject_Get_Property(binaryen binary_dir)
 
 file(MAKE_DIRECTORY ${source_dir}/src)
 
-if (WIN32)
-    add_library(libbinaryen SHARED IMPORTED)
-
-    set_target_properties(libbinaryen
-        PROPERTIES
-        IMPORTED_LOCATION ${binary_dir}/bin/libbinaryen.dll
-        IMPORTED_IMPLIB ${binary_dir}/lib/libbinaryen.dll.a
-        INTERFACE_INCLUDE_DIRECTORIES ${source_dir}/src
-    )
-else ()
-    add_library(libbinaryen STATIC IMPORTED)
-
-    set_target_properties(libbinaryen
-        PROPERTIES
-        IMPORTED_LOCATION ${binary_dir}/lib/libbinaryen.a
-        INTERFACE_INCLUDE_DIRECTORIES ${source_dir}/src
-    )
-endif ()
-
+add_library(libbinaryen STATIC IMPORTED)
 add_dependencies(libbinaryen binaryen)
+
+set_target_properties(libbinaryen
+    PROPERTIES
+    IMPORTED_LOCATION ${binary_dir}/lib/libbinaryen.a
+    INTERFACE_INCLUDE_DIRECTORIES ${source_dir}/src
+)
+
+# target_link_libraries(libbinaryen INTERFACE
+# )
